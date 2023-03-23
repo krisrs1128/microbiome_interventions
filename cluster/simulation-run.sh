c40501ce5b2dc5c5778af84198e5d6199040fd3f
Kris Sankaran <sankaran.kris@gmail.com>
2023-03-22 21:19:29 -0500

    docker permissions

--- cluster/simulation-run.sh
+++ cluster/simulation-run.sh
@@ -1,5 +1,5 @@
 
-# docker run --user $(id -u):$(id -g) --rm=true -it -v $(pwd):/scratch -w /scratch 5713f1a635aa /bin/bash
+# docker run --user $(id -u):$(id -g) --rm=true -it -v $(pwd):/scratch -w /scratch 708baa7eb51d /bin/bash
 
 Rscript -e "purrr::map(c('mbtransfer', 'mdsine', 'tfPaper'), devtools::install); mdsine::install_mdsine()"
 Rscript -e "mdsine::install_mdsine()"
