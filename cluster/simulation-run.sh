fe5db5ad491833f0a4121eb8a73d26c60a9fb7f4
Kris Sankaran <sankaran.kris@gmail.com>
2023-03-23 22:26:39 -0500

    update cluster simulation script

--- cluster/simulation-run.sh
+++ cluster/simulation-run.sh
@@ -1,9 +1,13 @@
 
 # docker run --user $(id -u):$(id -g) --rm=true -it -v $(pwd):/scratch -w /scratch 19e1c2fc5a7d /bin/bash
 
+# copy the data over
+export RUN=$(printf %03d $PROCESS)
+cp /staging/groups/sankaran_group/microbiome_interventions/sim_input_${RUN}.rda .
+
+# install packages
 Rscript -e "purrr::map(c('mbtransfer', 'mdsine', 'tfPaper'), devtools::install); mdsine::install_mdsine()"
 Rscript -e "mdsine::install_mdsine()"
 
-cd scripts
-Rscript -e "rmarkdown::render('simulation_data.Rmd')"
-Rscript -e "rmarkdown::render('simulation_metrics.Rmd')"
+# run the model configuration
+Rscript -e "rmarkdown::render('scripts/simulation_metrics.Rmd', params = list(data = 'sim_input_${RUN}.rda', run_id=${PROCESS}))"
