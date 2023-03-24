48b77591584edf1ad4d11cc05f063ed1d2cbac3d
Kris Sankaran <sankaran.kris@gmail.com>
2023-03-24 08:51:37 -0500

    copy result back

--- cluster/simulation-run.sh
+++ cluster/simulation-run.sh
@@ -13,5 +13,6 @@ for i in $(seq $((10 * PROCESS + 1)) $((10 * (PROCESS + 1)))); do
   export RUN=$(printf %03d $i)
   cp /staging/groups/sankaran_group/microbiome_interventions/sim_input_${RUN}.rda .
   Rscript -e "rmarkdown::render('simulation_metrics.Rmd', params = list(data = 'sim_input_${RUN}.rda', run_id=${i}))"
+  cp result*.rda /staging/groups/sankaran_group/microbiome_interventions/
 done
 
