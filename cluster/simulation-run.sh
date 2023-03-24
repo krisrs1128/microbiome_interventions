8e2c72a8a7c091df51a09a7c4a5420d184ebdd8c
Kris Sankaran <sankaran.kris@gmail.com>
2023-03-24 09:34:45 -0500

    update paths for simulation

--- cluster/simulation-run.sh
+++ cluster/simulation-run.sh
@@ -5,7 +5,6 @@
 # install packages
 Rscript -e "purrr::map(c('mbtransfer', 'mdsine', 'tfPaper'), devtools::install); mdsine::install_mdsine()"
 Rscript -e "mdsine::install_mdsine()"
-cd scripts
 
 # copy over data  
 cp /staging/ksankaran/microbiome_interventions/tf_sim.tar.gz .
@@ -15,10 +14,10 @@ tar -zxvf tf_sim.tar.gz
 PROCESS=$((PROCESS - 1))
 for i in $(seq $((10 * PROCESS + 1)) $((10 * (PROCESS + 1)))); do
   export RUN=$(printf %03d $i)
-  Rscript -e "rmarkdown::render('simulation_metrics.Rmd', params = list(data = 'sim_input_${RUN}.rda', run_id=${i}))"
+  Rscript -e "rmarkdown::render('scripts/simulation_metrics.Rmd', params = list(data = 'tf_sim/sim_input_${RUN}.rda', run_id=${i}))"
 done
 
 mkdir result-${PROCESS}
-mv result* result-${PROCESS}
+mv result*rda result-${PROCESS}
 tar -zcvf tf_sim_result-${PROCESS}.tar.gz result-${PROCESS}
 cp tf_sim_result*tar.gz /staging/ksankaran/microbiome_interventions/
