ec9131bc147d2243c56bc0e6a25837570495ce23
Kris Sankaran <sankaran.kris@gmail.com>
2023-03-29 17:32:55 -0500

    submit

--- cluster/simulation-run.sh
+++ cluster/simulation-run.sh
@@ -1,8 +1,8 @@
 
 # docker run --user $(id -u):$(id -g) --rm=true -it -v $(pwd):/scratch -w /scratch 19e1c2fc5a7d /bin/bash
 
-
 # install packages
+export batch_size=10
 tar -zxvf microbiome_interventions.tar.gz
 cd microbiome_interventions
 Rscript -e "purrr::map(c('mbtransfer', 'mdsine', 'tfPaper'), devtools::install)"
@@ -13,7 +13,7 @@ cp /staging/ksankaran/microbiome_interventions/tf_sim.tar.gz .
 tar -zxvf tf_sim.tar.gz
 
 # run the model configuration
-for i in $(seq $((10 * process + 1)) $((10 * (process + 1)))); do
+for i in $(seq $((batch_size * process + 1)) $((batch_size * (process + 1)))); do
   export RUN=$(printf %03d $i)
   Rscript -e "rmarkdown::render('scripts/simulation_metrics.Rmd', params = list(data = 'tf_sim/sim_input_${RUN}.rda', run_id=${i}))"
 done
