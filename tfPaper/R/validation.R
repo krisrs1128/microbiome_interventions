d824637dd87badda45c735214376e13d3d899d33
Kris Sankaran <sankaran.kris@gmail.com>
2023-03-28 23:07:26 -0500

    support evaluation at multiple lags

--- tfPaper/R/validation.R
+++ tfPaper/R/validation.R
@@ -6,26 +6,25 @@ train_test_split <- function(ts_inter, p_test = 0.2) {
   list(train = ts_inter[!test_ix], test = ts_inter[test_ix])
 }
 
-#' @export
-post_intervention <- function(interventions, window) {
-  result <- rep(FALSE, length(interventions))
-  for (i in seq_along(result)) {
-    if (any(interventions[max(i - window, 1):i] == 1)) {
-      result[i] <- TRUE
-    }
+intervention_start <- function(interventions, offset = 0) {
+  perturbed <- colSums(interventions) > 0
+  if (any(head(perturbed, offset - 1))) {
+    start_ix <- min(which(perturbed))
+  } else {
+    start_ix <- ncol(interventions) / 2
   }
   
-  result
+  start_ix + offset
 }
 
 #' @importFrom dplyr bind_rows
 #' @export
-evaluation <- function(y, y_hat, test_ix, window = 10) {
+evaluation <- function(y, y_hat, test_ix) {
   residuals <- list()
   avg_err <- list()
   
   for (i in seq_along(y)) {
-    residuals[[i]]<- values(y[[i]][, test_ix]) - values(y_hat[[i]][, test_ix])
+    residuals[[i]]<- values(y[[i]][, test_ix[[i]]]) - values(y_hat[[i]][, test_ix[[i]]])
     avg_err[[i]] <- data.frame(
       mse = mean(residuals[[i]] ^ 2),
       mae = mean(abs(residuals[[i]]))
@@ -35,10 +34,10 @@ evaluation <- function(y, y_hat, test_ix, window = 10) {
   bind_rows(avg_err, .id = "subject")
 }
 
-#' @importFrom magrittr %>%
 #' @importFrom dplyr bind_rows
+#' @importFrom tidyr separate
 #' @export
-cross_validate <- function(ts_inter, train, K = 5, n_ahead = 5, t_start = 5) {
+cross_validate <- function(ts_inter, train, K = 5, n_ahead = 5, offset = -1) {
   N <- length(ts_inter)
 
   fits <- list()
@@ -50,16 +49,26 @@ cross_validate <- function(ts_inter, train, K = 5, n_ahead = 5, t_start = 5) {
     fits[[k]] <- train(splits$train)
 
     stest <- splits$test
-    for (i in seq_along(splits$test)) {
-      values(stest[[i]]) <- values(stest[[i]])[, seq_len(t_start), drop = FALSE]
-      interventions(stest[[i]]) <- interventions(stest[[i]])[, seq_len(t_start + n_ahead), drop = FALSE]
+    t_starts <- vector(length = length(stest))
+    for (i in seq_along(stest)) {
+      t_starts[i] <- intervention_start(interventions(stest[[i]]), offset)
+      values(stest[[i]]) <- values(stest[[i]])[, seq_len(t_starts[i]), drop = FALSE]
+      predict_ix <- seq_len(min(t_starts[i] + n_ahead, ncol(splits$test[[i]])))
+      interventions(stest[[i]]) <- interventions(stest[[i]])[, predict_ix, drop = FALSE]
     }
     
     y_hat[[k]] <- predict(fits[[k]], stest)
-    test_ix <- seq(t_start, t_start + n_ahead)
-    metrics[[k]] <- evaluation(splits$test, y_hat[[k]], test_ix, k)
+    for (h in seq_len(n_ahead)) {
+      test_ix <- list()
+      for (i in seq_along(stest)) {
+        test_ix[[i]] <- seq(t_starts[i], min(t_starts[i] + h, ncol(y_hat[[k]][[i]])))
+      }
+      
+      metrics[[glue("{k}-{h}")]] <- evaluation(splits$test, y_hat[[k]], test_ix)
+    }
   }
   
-  metrics <- bind_rows(metrics, .id = "fold")
+  metrics <- bind_rows(metrics, .id = "fold_lag") |>
+    separate(fold_lag, c("fold", "lag"))
   list(fits = fits, y_hat = y_hat, metrics = metrics)
 }
