2d3a7cec93f9960cabd8b29e59e377affb6d13bc
Kris Sankaran <sankaran.kris@gmail.com>
2023-04-03 21:31:52 -0500

    row-wise subsetting of ts-inter objects

--- mbtransfer/R/class-definitions.R
+++ mbtransfer/R/class-definitions.R
@@ -19,20 +19,29 @@ setClass(
   )
 )
 
-single_subset <- function(x, i, j, ..., drop = FALSE) {
-  values <- x@values[i, j, drop = drop]
+single_subset <- function(x, i = NULL, j = NULL, ..., drop = FALSE) {
+  if (!missing(i)) {
+    values <- x@values[i, j, drop = drop]
+  } else {
+    values <- x@values[, j, drop = drop]
+  }
+
   time <- x@time[j]
   interventions <- x@interventions[, j, drop = drop]
   new("ts_inter_single", values = values, time = time, interventions = interventions)
 }
 
-multi_subset <- function(x, i, j, ..., drop = FALSE) {
+multi_subset <- function(x, i = NULL, j = NULL, ..., drop = FALSE) {
   result <- list()
   for (k in seq_along(x)) {
-    result[[k]] <- x[[k]][i, j]
+    if (!missing(i)) {
+      result[[k]] <- x[[k]][i, j]
+    } else {
+      result[[k]] <- x[[k]][, j]
+    }
   }
   
-  new("ts_inter", series = result)
+  new("ts_inter", series = result, subject_data = x@subject_data)
 }
 
 setMethod("length", "ts_inter", function(x) length(x@series))
@@ -44,10 +53,12 @@ setMethod("dim", "ts_inter_single", function(x) dim(x@values))
 setMethod("[", c("ts_inter_single", "numeric", "numeric"), single_subset)
 setMethod("[", c("ts_inter_single", "numeric", "missing"), single_subset)
 setMethod("[", c("ts_inter_single", "missing", "numeric"), single_subset)
-setMethod("[", c("ts_inter", "numeric", "missing", "ANY"), function(x, i, j, ..., drop=TRUE) initialize(x, series=x@series[i]))
+setMethod("[", c("ts_inter", "numeric", "missing", "ANY"), function(x, i, j, ..., drop=TRUE) initialize(x, series=x@series[i], subject_data = x@subject_data[i, ]))
 setMethod("[[", c("ts_inter", "numeric", "missing"), function(x, i, j, ...) x@series[[i]])
-setMethod("[", c("ts_inter", "logical", "missing", "ANY"), function(x, i, j, ..., drop=TRUE) initialize(x, series=x@series[i]))
+setMethod("[", c("ts_inter", "logical", "missing", "ANY"), function(x, i, j, ..., drop=TRUE) initialize(x, series=x@series[i], subject_data = x@subject_data[i, ]))
 setMethod("[[", c("ts_inter", "logical", "missing"), function(x, i, j, ...) x@series[[i]])
+setMethod("[", c("ts_inter", "missing", "numeric", "ANY"), multi_subset)
+setMethod("[", c("ts_inter", "numeric", "numeric", "ANY"), multi_subset)
 
 #' @export
 setGeneric("values", function(x) standardGeneric("values"))
@@ -94,3 +105,9 @@ setMethod("subject_data<-", "ts_inter", function(x, value) {
 
 #' @export
 setMethod("names", "ts_inter", function(x) names(x@series))
+
+#' @export
+setMethod("names<-", "ts_inter", function(x, value) { 
+  names(x@series) <- value 
+  x
+})
