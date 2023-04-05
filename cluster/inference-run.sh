8976a34ea5f814bb87e857f3611acca264635daf
Kris Sankaran <sankaran.kris@gmail.com>
2023-04-05 17:56:46 -0500

    split simulation types

---
+++ cluster/inference-run.sh
@@ -0,0 +1,25 @@
+
+# docker run --user $(id -u):$(id -g) --rm=true -it -v $(pwd):/scratch -w /scratch 19e1c2fc5a7d /bin/bash
+
+# install packages
+export batch_size=2
+tar -zxvf microbiome_interventions.tar.gz
+cd microbiome_interventions
+Rscript -e "purrr::map(c('mbtransfer', 'tfPaper')"
+Rscript -e "mdsine::install_mdsine()"
+
+# copy over data  
+cp /staging/ksankaran/microbiome_interventions/tf_sim.tar.gz .
+tar -zxvf tf_sim.tar.gz
+
+# run the model configuration
+for i in $(seq $((batch_size * process + 1)) $((batch_size * (process + 1)))); do
+  export RUN=$(printf %03d $i)
+  Rscript -e "rmarkdown::render('scripts/inference_metrics.Rmd', params = list(data_dir = '../tf_sim/', run_id=${RUN}))"
+done
+
+mkdir result-${process}
+mv scripts/result*rda result-${process}
+mv scripts/*html result-${process}
+tar -zcvf inference_result-${process}.tar.gz inference-${process}
+cp inference_result*tar.gz /staging/ksankaran/microbiome_interventions/
