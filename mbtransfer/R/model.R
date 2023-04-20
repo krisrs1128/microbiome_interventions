3481cfb89f012065f5b92fe8be00fcf222777d77
Kris Sankaran <sankaran.kris@gmail.com>
2023-04-19 23:13:44 -0500

    assume times equal intervention times

--- mbtransfer/R/model.R
+++ mbtransfer/R/model.R
@@ -61,7 +61,6 @@ model_predict_step <- function(ts_inter, fit, lags, subject = NULL) {
 
   values(ts_inter) <- cbind(values(ts_inter), y_hat)
   delta <- median(diff(ts_inter@time))
-  ts_inter@time <- c(ts_inter@time, max(ts_inter@time) + delta)
   ts_inter
 }
 
