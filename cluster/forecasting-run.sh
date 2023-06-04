7f2dfcd4a0b851d7e0efa78d4b06cc270a725abe
Kris Sankaran <sankaran.kris@gmail.com>
2023-06-04 18:16:48 -0500

    Rerun held jobs on the cluster

--- cluster/forecasting-run.sh
+++ cluster/forecasting-run.sh
@@ -12,6 +12,10 @@ Rscript -e "mdsine::install_mdsine()"
 cp /staging/ksankaran/microbiome_interventions/tf_sim.tar.gz .
 tar -zxvf tf_sim.tar.gz
 
+# used to rerun high memory jobs
+export run_ids=('271' '272' '275' '276' '279' '280' '283' '284' '287' '288' '291' '292' '295' '296' '299' '300' '303' '304' '307' '308' '311' '312' '315' '316' '319' '320' '323' '324' '327' '328' '331' '332' '335' '336' '339' '340' '343' '344' '347' '348' '351' '352' '355' '356' '359' '360' '363' '364' '367' '368' '371' '372' '375' '376')
+process=${run_ids[process]}
+
 # run the model configuration
 for i in $(seq $((batch_size * process + 1)) $((batch_size * (process + 1)))); do
   export RUN=$(printf %03d $i)
