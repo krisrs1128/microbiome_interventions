a5a04a5281a781992290e3601076138984c69575
Kris Sankaran <sankaran.kris@gmail.com>
2024-04-06 17:14:10 -0500

    switch installation order

--- cluster/forecasting-run.sh
+++ cluster/forecasting-run.sh
@@ -6,11 +6,11 @@ export batch_size=3
 tar -zxvf microbiome_interventions.tar.gz
 cd microbiome_interventions
 Rscript -e "install.packages('fido', repos='https://cloud.r-project.org')"
-Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/mdsine')"
 Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/tfPaper')"
 Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/fido2')"
 Rscript -e "devtools::install_github('gathanei/xyz')"
 Rscript -e "devtools::install_github('krisrs1128/mbtransfer')"
+Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/mdsine')"
 Rscript -e "mdsine::install_mdsine()"
 
 # copy over data
