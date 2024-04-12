8bf833b602648b06f74b185ec9b516f09ba7bce5
Kris Sankaran <sankaran.kris@gmail.com>
2024-04-12 17:11:10 -0500

    try many smaller, low-cost runs

--- cluster/forecasting-run.sh
+++ cluster/forecasting-run.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 # docker run --user $(id -u):$(id -g) --rm=true -it -v $(pwd):/scratch -w /scratch 19e1c2fc5a7d /bin/bash
 
-# install packages
-export batch_size=3
-tar -zxvf microbiome_interventions.tar.gz
-cd microbiome_interventions
-Rscript -e "install.packages('fido', repos='https://cloud.r-project.org')"
-Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/tfPaper')"
-Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/fido2')"
-Rscript -e "devtools::install_github('gathanei/xyz')"
-Rscript -e "devtools::install_github('krisrs1128/mbtransfer')"
-Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/mdsine')"
-Rscript -e "mdsine::install_mdsine()"
+# copy and check if results already exist
+cp /staging/ksankaran/microbiome_interventions/forecasting-${process}.* .
+tar -zxvf forecasting-${process}.tar.gz
 
-# copy over data
-cp /staging/ksankaran/microbiome_interventions/tf_sim.tar.gz .
-tar -zxvf tf_sim.tar.gz
+file_num=$(ls -l forecasting-${process}/*.rda | wc -l)
+if [[ $file_num -lt 1 ]]; then
 
-# used to rerun high memory jobs
-#export run_ids=('271' '275' '279' '283' '287' '291' '295' '299' '303' '307' '311' '315' '319' '323' '327' '331' '335' '339' '343' '347' '351' '355' '359' '363' '367' '371' '375')
-#process=${run_ids[process]}
+  # install packages
+  export batch_size=1
+  tar -zxvf microbiome_interventions.tar.gz
+  cd microbiome_interventions
+  Rscript -e "install.packages('fido', repos='https://cloud.r-project.org')"
+  Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/tfPaper')"
+  Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/fido2')"
+  Rscript -e "devtools::install_github('gathanei/xyz')"
+  Rscript -e "devtools::install_github('krisrs1128/mbtransfer')"
+  Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/mdsine')"
+  Rscript -e "mdsine::install_mdsine()"
 
-# run the model configuration
-for i in $(seq $((batch_size * process + 1)) $((batch_size * (process + 1)))); do
-  export RUN=$(printf %03d $i)
-  Rscript -e "rmarkdown::render('scripts/forecasting_metrics.Rmd', params = list(data_dir = '../tf_sim/', run_id=${RUN}))"
-done
+  # copy over data
+  cp /staging/ksankaran/microbiome_interventions/tf_sim.tar.gz .
+  tar -zxvf tf_sim.tar.gz
 
-mkdir forecasting-${process}
-mv scripts/*rda forecasting-${process}
-mv scripts/*html forecasting-${process}
-tar -zcvf forecasting-${process}.tar.gz forecasting-${process}
-cp forecasting-*tar.gz /staging/ksankaran/microbiome_interventions/
+  # run the model configuration
+  for i in $(seq $((batch_size * process + 1)) $((batch_size * (process + 1)))); do
+    export RUN=$(printf %03d $i)
+    Rscript -e "rmarkdown::render('scripts/forecasting_metrics.Rmd', params = list(data_dir = '../tf_sim/', run_id=${RUN}))"
+  done
+
+  mkdir forecasting-${process}
+  mv scripts/*rda forecasting-${process}
+  mv scripts/*html forecasting-${process}
+  tar -zcvf forecasting-${process}.tar.gz forecasting-${process}
+  cp forecasting-*tar.gz /staging/ksankaran/microbiome_interventions/
+fi
--- cluster/inference-run.sh
+++ cluster/inference-run.sh
@@ -1,28 +1,37 @@
 
 # docker run --user $(id -u):$(id -g) --rm=true -it -v $(pwd):/scratch -w /scratch 19e1c2fc5a7d /bin/bash
 
-# install packages
-export batch_size=1
-tar -zxvf microbiome_interventions.tar.gz
-cd microbiome_interventions
-Rscript -e "devtools::install('tfPaper')"
-Rscript -e "devtools::install_github('gathanei/xyz')"
-Rscript -e "devtools::install_github('krisrs1128/mbtransfer')"
+# copy and check if results already exist
+cp /staging/ksankaran/microbiome_interventions/inference-${process}.* .
+tar -zxvf inference-${process}.tar.gz
 
-# copy over data  
-cp /staging/ksankaran/microbiome_interventions/tf_sim.tar.gz .
-tar -zxvf tf_sim.tar.gz
+file_num=$(ls -l inference-${process}/*.rda | wc -l)
+if [[ $file_num -lt 1 ]]; then
 
-#process=$((process + 54))
+  # install packages
+  export batch_size=1
+  tar -zxvf microbiome_interventions.tar.gz
+  cd microbiome_interventions
+  Rscript -e "devtools::install('tfPaper')"
+  Rscript -e "devtools::install_github('gathanei/xyz')"
+  Rscript -e "devtools::install_github('krisrs1128/mbtransfer')"
 
-# run the model configuration
-for i in $(seq $((batch_size * process + 1)) $((batch_size * (process + 1)))); do
-  export RUN=$(printf %03d $i)
-  Rscript -e "rmarkdown::render('scripts/inference_metrics.Rmd', params = list(data_dir = '../tf_sim/', run_id=${RUN}))"
-done
+  # copy over data  
+  cp /staging/ksankaran/microbiome_interventions/tf_sim.tar.gz .
+  tar -zxvf tf_sim.tar.gz
 
-mkdir inference-${process}
-mv scripts/*rda inference-${process}
-mv scripts/*html inference-${process}
-tar -zcvf inference_result-${process}.tar.gz inference-${process}
-cp inference_result*tar.gz /staging/ksankaran/microbiome_interventions/
+  #process=$((process + 54))
+
+  # run the model configuration
+  for i in $(seq $((batch_size * process + 1)) $((batch_size * (process + 1)))); do
+    export RUN=$(printf %03d $i)
+    Rscript -e "rmarkdown::render('scripts/inference_metrics.Rmd', params = list(data_dir = '../tf_sim/', run_id=${RUN}))"
+  done
+
+  mkdir inference-${process}
+  mv scripts/*rda inference-${process}
+  mv scripts/*html inference-${process}
+  tar -zcvf inference_result-${process}.tar.gz inference-${process}
+  cp inference_result*tar.gz /staging/ksankaran/microbiome_interventions/
+
+fi;
