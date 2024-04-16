b304474838bae50121ad97b1cbac8565102b7147
Kris Sankaran <sankaran.kris@gmail.com>
2024-04-16 17:51:34 -0500

    assume tf-sim

--- cluster/forecasting-run.sh
+++ cluster/forecasting-run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # docker run --user $(id -u):$(id -g) --rm=true -it -v $(pwd):/scratch -w /scratch 19e1c2fc5a7d /bin/bash
   
+# check if results already exist
 tar -zxvf outputs.tar.gz
-
-# copy and check if results already exist
 test_output=outputs/forecasting-${process}.tar.gz
 if [ -f $test_output ]; then
   cp $test_output .
@@ -27,11 +26,8 @@ if [[ $file_num -lt 1 ]]; then
   Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/mdsine')"
   Rscript -e "mdsine::install_mdsine()"
 
-  # copy over data
-  cp /staging/ksankaran/microbiome_interventions/tf_sim.tar.gz .
-  tar -zxvf tf_sim.tar.gz
-
   # run the model configuration
+  tar -zxvf tf_sim.tar.gz
   for i in $(seq $((batch_size * process + 1)) $((batch_size * (process + 1)))); do
     export RUN=$(printf %03d $i)
     Rscript -e "rmarkdown::render('scripts/forecasting_metrics.Rmd', params = list(data_dir = '../tf_sim/', run_id=${RUN}))"
