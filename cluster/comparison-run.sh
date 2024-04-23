0c1786f2554d9697341c00279a0fe6bd7f24e689
Kris Sankaran <sankaran.kris@gmail.com>
2024-04-23 08:54:55 -0500

    try running single run on cluster

---
+++ cluster/comparison-run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# docker run --user $(id -u):$(id -g) --rm=true -it -v $(pwd):/scratch -w /scratch 19e1c2fc5a7d /bin/bash
+  
+# install packages
+tar -zxvf microbiome_interventions.tar.gz
+cd microbiome_interventions
+Rscript -e "install.packages('fido', repos='https://cloud.r-project.org')"
+Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/tfPaper')"
+Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/fido2')"
+Rscript -e "devtools::install_github('gathanei/xyz')"
+Rscript -e "devtools::install_github('krisrs1128/mbtransfer')"
+Rscript -e "devtools::install_github('krisrs1128/microbiome_interventions/mdsine')"
+Rscript -e "mdsine::install_mdsine()"
+
+# run the model configuration
+cp ../tf_sim.tar.gz .
+tar -zxvf tf_sim.tar.gz
+Rscript -e "rmarkdown::render('scripts/compare_model_trajectories.Rmd', params = list(data_dir = '../tf_sim/', run_id=${RUN}))"
+done
+
+mkdir comparison-run
+mv scripts/*rda comparison-run
+mv scripts/*html comparison-run
+mv scripts/*rds comparison-run
+tar -zcvf comparison-run.tar.gz comparison-run
+cp comparison-run.tar.gz ../
